3345f63c07b7278655ddcf3cb86dc9b86142b6af
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2024-10-02 20:12:04 +0200

    Code formatting.

--- inc/dps310.h
+++ inc/dps310.h
@@ -21,19 +21,19 @@
  * \brief DPS310 driver error codes.
  *******************************************************************/
 typedef enum {
-	// Driver errors.
-	DPS310_SUCCESS = 0,
-	DPS310_ERROR_NULL_PARAMETER,
-	DPS310_ERROR_COEFFICIENTS_TIMEOUT,
-	DPS310_ERROR_SENSOR_TIMEOUT,
-	DPS310_ERROR_TEMPERATURE_TIMEOUT,
-	DPS310_ERROR_PRESSURE_TIMEOUT,
-	// Low level drivers errors.
-	DPS310_ERROR_BASE_I2C = 0x0100,
-	DPS310_ERROR_BASE_DELAY = (DPS310_ERROR_BASE_I2C + DPS310_DRIVER_I2C_ERROR_BASE_LAST),
-	DPS310_ERROR_BASE_MATH = (DPS310_ERROR_BASE_DELAY + DPS310_DRIVER_DELAY_ERROR_BASE_LAST),
-	// Last base value.
-	DPS310_ERROR_BASE_LAST = (DPS310_ERROR_BASE_MATH + MATH_ERROR_BASE_LAST)
+    // Driver errors.
+    DPS310_SUCCESS = 0,
+    DPS310_ERROR_NULL_PARAMETER,
+    DPS310_ERROR_COEFFICIENTS_TIMEOUT,
+    DPS310_ERROR_SENSOR_TIMEOUT,
+    DPS310_ERROR_TEMPERATURE_TIMEOUT,
+    DPS310_ERROR_PRESSURE_TIMEOUT,
+    // Low level drivers errors.
+    DPS310_ERROR_BASE_I2C = 0x0100,
+    DPS310_ERROR_BASE_DELAY = (DPS310_ERROR_BASE_I2C + DPS310_DRIVER_I2C_ERROR_BASE_LAST),
+    DPS310_ERROR_BASE_MATH = (DPS310_ERROR_BASE_DELAY + DPS310_DRIVER_DELAY_ERROR_BASE_LAST),
+    // Last base value.
+    DPS310_ERROR_BASE_LAST = (DPS310_ERROR_BASE_MATH + MATH_ERROR_BASE_LAST)
 } DPS310_status_t;
 
 #ifndef DPS310_DRIVER_DISABLE
@@ -43,28 +43,28 @@ typedef enum {
 /*!******************************************************************
  * \fn DPS310_status_t DPS310_init(void)
  * \brief Init DPS310 driver.
- * \param[in]  	none
- * \param[out] 	none
- * \retval		Function execution status.
+ * \param[in]   none
+ * \param[out]  none
+ * \retval      Function execution status.
  *******************************************************************/
 DPS310_status_t DPS310_init(void);
 
 /*!******************************************************************
  * \fn DPS310_status_t DPS310_de_init(void)
  * \brief Release DPS310 driver.
- * \param[in]  	none
- * \param[out] 	none
- * \retval		Function execution status.
+ * \param[in]   none
+ * \param[out]  none
+ * \retval      Function execution status.
  *******************************************************************/
 DPS310_status_t DPS310_de_init(void);
 
 /*!******************************************************************
  * \fn DPS310_status_t DPS310_get_pressure_temperature(uint8_t i2c_address, int32_t* pressure_pa, int32_t* temperature_degrees)
  * \brief Perform pressure and temperature measurements.
- * \param[in]  	i2c_address: I2C address of the sensor.
- * \param[out] 	pressure_pa: Pointer to integer that will contain the atmospheric pressure in Pa.
- * \param[out]	temperature_degrees: Pointer to integer that will contain the temperature in Celsius degrees.
- * \retval		Function execution status.
+ * \param[in]   i2c_address: I2C address of the sensor.
+ * \param[out]  pressure_pa: Pointer to integer that will contain the atmospheric pressure in Pa.
+ * \param[out]  temperature_degrees: Pointer to integer that will contain the temperature in Celsius degrees.
+ * \retval      Function execution status.
  *******************************************************************/
 DPS310_status_t DPS310_get_pressure_temperature(uint8_t i2c_address, int32_t* pressure_pa, int32_t* temperature_degrees);
 
--- inc/dps310_hw.h
+++ inc/dps310_hw.h
@@ -21,49 +21,49 @@
 /*!******************************************************************
  * \fn DPS310_status_t DPS310_HW_init(void)
  * \brief Init DPS310 hardware interface.
- * \param[in]  	none
- * \param[out] 	none
- * \retval		Function execution status.
+ * \param[in]   none
+ * \param[out]  none
+ * \retval      Function execution status.
  *******************************************************************/
 DPS310_status_t DPS310_HW_init(void);
 
 /*!******************************************************************
  * \fn DPS310_status_t DPS310_HW_de_init(void)
  * \brief Release DPS310 hardware interface.
- * \param[in]  	none
- * \param[out] 	none
- * \retval		Function execution status.
+ * \param[in]   none
+ * \param[out]  none
+ * \retval      Function execution status.
  *******************************************************************/
 DPS310_status_t DPS310_HW_de_init(void);
 
 /*!******************************************************************
  * \fn DPS310_status_t DPS310_HW_i2c_write(uint8_t i2c_address, uint8_t* data, uint8_t data_size_bytes, uint8_t stop_flag)
  * \brief Write data to sensor over I2C bus.
- * \param[in]  	i2c_address: 7-bits sensor address.
- * \param[in]	data: Byte array to send.
- * \param[in]	data_size_bytes: Number of bytes to send.
- * \param[in]	stop_flag: Generate stop condition at the end of the transfer if non zero.
- * \param[out] 	none
- * \retval		Function execution status.
+ * \param[in]   i2c_address: 7-bits sensor address.
+ * \param[in]   data: Byte array to send.
+ * \param[in]   data_size_bytes: Number of bytes to send.
+ * \param[in]   stop_flag: Generate stop condition at the end of the transfer if non zero.
+ * \param[out]  none
+ * \retval      Function execution status.
  *******************************************************************/
 DPS310_status_t DPS310_HW_i2c_write(uint8_t i2c_address, uint8_t* data, uint8_t data_size_bytes, uint8_t stop_flag);
 
 /*!******************************************************************
  * \fn DPS310_status_t DPS310_HW_i2c_read(uint8_t i2c_address, uint8_t* data, uint8_t data_size_bytes)
  * \brief Read data from sensor over I2C bus.
- * \param[in]  	i2c_address: 7-bits sensor address.
- * \param[in]	data_size_bytes: Number of bytes to read.
- * \param[out]	data: Byte array that will contain the read data.
- * \retval		Function execution status.
+ * \param[in]   i2c_address: 7-bits sensor address.
+ * \param[in]   data_size_bytes: Number of bytes to read.
+ * \param[out]  data: Byte array that will contain the read data.
+ * \retval      Function execution status.
  *******************************************************************/
 DPS310_status_t DPS310_HW_i2c_read(uint8_t i2c_address, uint8_t* data, uint8_t data_size_bytes);
 
 /*!******************************************************************
  * \fn DPS310_status_t DPS310_HW_delay_milliseconds(uint32_t delay_ms)
  * \brief Delay function.
- * \param[in]  	delay_ms: Delay to wait in ms.
- * \param[out] 	none
- * \retval		Function execution status.
+ * \param[in]   delay_ms: Delay to wait in ms.
+ * \param[out]  none
+ * \retval      Function execution status.
  *******************************************************************/
 DPS310_status_t DPS310_HW_delay_milliseconds(uint32_t delay_ms);
 
